c36f2efc1be534a4d47efdbebd82a20065f2d063
wilffren <wilffren14@gmail.com>
2025-12-09 14:18:45 -0500

    Feat: Grafana was implement

--- coopcredit-system/credit-application-service/src/main/java/com/coopcredit/creditapplication/infrastructure/config/OpenApiConfig.java
+++ coopcredit-system/credit-application-service/src/main/java/com/coopcredit/creditapplication/infrastructure/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.coopcredit.creditapplication.infrastructure.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +17,16 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("CoopCredit API")
                         .version("1.0")
-                        .description("Credit Application Service API for Cooperative Credit System"))
+                        .description("Credit Application Service API for Cooperative Credit System. " +
+                                "Use /api/auth/register to create a user, then /api/auth/login to get a JWT token. " +
+                                "Click 'Authorize' and enter: Bearer <your-token>"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components()
                         .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("bearerAuth")
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
-                                .bearerFormat("JWT")));
+                                .bearerFormat("JWT")
+                                .description("Enter JWT token with Bearer prefix")));
     }
 }
